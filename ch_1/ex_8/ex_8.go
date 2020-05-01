4ef8e347175e558cdc9dd7011f9b76a46f28b268
Angus Morrison <contact@angus-morrison.com>
2020-05-01 06:48:58 +0100

    ex_8

---
+++ ch_1/ex_8/ex_8.go
@@ -0,0 +1,34 @@
+/*
+Modify fetch to add teh prefix http:// to each argument URL if it is missing.
+You ight what to use strings.HasPrefix.
+*/
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", body)
+	}
+}
