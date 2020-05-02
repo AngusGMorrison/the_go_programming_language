139da5a42de27b3d70827271027d27f0a594257b
Angus Morrison <contact@angus-morrison.com>
2020-05-02 19:17:49 +0100

    Fixed file in wrong location

--- ch3/ex1/ex1.go
+++ ch3/ex1/ex1.go
@@ -1,7 +1,6 @@
 /*
-If the function f returns a non-finite float64 value, the SVG file will contain
-invalid <polygon> elements (although many SVG renderers handle this gracefully).
-Modify the program to skip invalid polygons.
+Color each polygon based on its height, so that the peaks are colored red (#ff0000) and the valleys
+blue (#0000ff).
 */
 
 package main
