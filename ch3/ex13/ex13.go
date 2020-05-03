b3bb2634b616949701f43b53dee0ff659cacb705
Angus Morrison <contact@angus-morrison.com>
2020-05-03 19:31:25 +0100

    ex13

---
+++ ch3/ex13/ex13.go
@@ -0,0 +1,16 @@
+/*
+Write const declarations for KB, MB up to YB as compactly as you can.
+*/
+
+package main
+
+const (
+	KB = 1000
+	MB = KB * KB
+	GB = MB * KB
+	TB = GB * KB
+	PB = TB * KB
+	EB = PB * KB
+	ZB = EB * KB
+	YB = ZB * KB
+)
