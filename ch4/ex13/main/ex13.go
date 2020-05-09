831d96a6d9cf72d0c39d618296339e0c6bc6fcc5
Angus Morrison <contact@angus-morrison.com>
2020-05-09 15:34:07 +0100

    ex13 skipped

---
+++ ch4/ex13/main/ex13.go
@@ -0,0 +1,5 @@
+// The JSON-based web service of the Open Movie Database lets you search https://omdbapi.com/ for a
+// movie by name and download its poster image. Write a tool poster that downloads the poster image
+// for the movie named on the command line.
+
+// omdb Poster API is now a paid-for service; excercise omitted.
