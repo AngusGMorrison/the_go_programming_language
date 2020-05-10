1c7c0090927ef3edcaf47c91ce7a0d2fbc6cb308
Angus Morrison <contact@angus-morrison.com>
2020-05-10 16:57:07 +0100

    ex6

---
+++ ch5/cmd/ex6/corner.go
@@ -0,0 +1,35 @@
+// Modify the corner function in gopl.io/ch3/surface (§3.2) to use named results and a bare return
+// statement.
+package main
+
+import "math"
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func corner(i, j int) (sx, sy float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	// Project (x, y, z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx = width/2 + (x-y)*cos30*xyscale
+	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
+	return
+}
+
+func f(x, y float64) (z float64) {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
