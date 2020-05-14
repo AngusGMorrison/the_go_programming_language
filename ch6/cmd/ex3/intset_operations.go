baf217259b49da6cef55422cec6c5dce78c08bd3
Angus Morrison <contact@angus-morrison.com>
2020-05-14 08:08:34 +0100

    ex3

---
+++ ch6/cmd/ex3/intset_operations.go
@@ -0,0 +1,139 @@
+// (*IntSet).UnionWith computes the union of two sets using |, the word-parallel bitwise OR
+// operator. Implement methods for IntersectWith, DifferenceWith, and SymetricDifference for the
+// corresponding set operations. (The symmetric difference of two sets contains the elements
+// present in one set or the other, but not both).
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// IntersectWith sets s to the intersection of s and t.
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, sword := range s.words {
+		if i < len(t.words) {
+			s.words[i] = sword & t.words[i]
+		} else {
+			s.words[i] = 0
+		}
+	}
+}
+
+// DifferenceWith sets s to the difference of s and t.
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, sword := range s.words {
+		if i >= len(t.words) {
+			break
+		}
+		s.words[i] = sword &^ t.words[i]
+	}
+}
+
+// SymmetricDifference sets s to the symmetric difference of s and t.
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] = s.words[i] ^ tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<j) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// Len returns the number of elements in s.
+func (s *IntSet) Len() int {
+	var len uint64
+	for _, word := range s.words {
+		for word > 0 {
+			len += word & 1
+			word >>= 1
+		}
+	}
+	return int(len)
+}
+
+// Remove removes x from s.
+func (s *IntSet) Remove(x int) {
+	word, bits := x/64, uint(x%64)
+	if len(s.words) < word {
+		return
+	}
+	s.words[word] &= ^(1 << bits)
+}
+
+// Clear removes all elements from s.
+func (s *IntSet) Clear() {
+	for i := range s.words {
+		s.words[i] = 0
+	}
+}
+
+// Copy returns a copy of s.
+func (s *IntSet) Copy() *IntSet {
+	copiedWords := make([]uint64, len(s.words))
+	for i, word := range s.words {
+		copiedWords[i] = word
+	}
+	return &IntSet{copiedWords}
+}
+
+// AddAll adds all of its arguments to the s.
+func (s *IntSet) AddAll(vals ...int) {
+	for _, val := range vals {
+		s.Add(val)
+	}
+}
---
+++ ch6/cmd/ex3/intset_operations_test.go
@@ -0,0 +1,101 @@
+package intset
+
+import (
+	"testing"
+)
+
+const errorFmt = "Expected %v, got %v"
+
+func createTestSet() *IntSet {
+	set := IntSet{}
+	set.AddAll(1, 2, 3)
+	return &set
+}
+
+func TestLen(t *testing.T) {
+	set := IntSet{}
+	var len int
+	if len = set.Len(); len != 0 {
+		t.Errorf(errorFmt, 0, len)
+	}
+	set.Add(3)
+	set.Add(2)
+	if len = set.Len(); len != 2 {
+		t.Errorf(errorFmt, 2, len)
+	}
+}
+
+func TestRemove(t *testing.T) {
+	set := createTestSet()
+	set.Remove(2)
+	if has := set.Has(2); has {
+		t.Errorf(errorFmt, false, has)
+	}
+}
+
+func TestClear(t *testing.T) {
+	set := createTestSet()
+	set.Clear()
+	if len := set.Len(); len > 0 {
+		t.Errorf(errorFmt, 0, len)
+	}
+}
+
+func TestCopy(t *testing.T) {
+	original := createTestSet()
+	copy := original.Copy()
+	if original == copy {
+		t.Errorf("Original and copy addresses are equal")
+	}
+	if &original.words == &copy.words {
+		t.Errorf("Original and copy use the same slice")
+	}
+}
+
+func TestAddAll(t *testing.T) {
+	set := createTestSet()
+	set.AddAll(4, 5, 6)
+	if !set.Has(4) || !set.Has(5) || !set.Has(6) {
+		t.Errorf("AddAll failed to add all args to set")
+	}
+}
+
+func TestIntersectWith(t *testing.T) {
+	set1 := createTestSet()
+	set2 := &IntSet{}
+	set2.Add(1)
+	set1.IntersectWith(set2)
+	if len := set1.Len(); len != 1 {
+		t.Errorf("Expected len 1, got len %d", len)
+	}
+	if !set1.Has(1) {
+		t.Errorf("1 not present in set")
+	}
+}
+
+func TestDifferenceWith(t *testing.T) {
+	set1 := createTestSet()
+	set2 := &IntSet{}
+	set2.AddAll(2, 3)
+	set1.DifferenceWith(set2)
+	if len := set1.Len(); len != 1 {
+		t.Errorf("Expected len 1, got len %d", len)
+	}
+	if !set1.Has(1) {
+		t.Errorf("1 not present in set")
+	}
+}
+
+func TestSymmetricDifference(t *testing.T) {
+	set1 := createTestSet()
+	set1.Add(4)
+	set2 := createTestSet()
+	set2.Add(6)
+	set1.SymmetricDifference(set2)
+	if len := set1.Len(); len != 2 {
+		t.Errorf("Expected len 2, got len %d", len)
+	}
+	if !set1.Has(4) || !set1.Has(6) {
+		t.Errorf("Unexpected elements in set")
+	}
+}
