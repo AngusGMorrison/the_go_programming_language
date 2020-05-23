0aca6cc621bfa8bc5ef1e97f21981e7f18b525a7
Angus Morrison <contact@angus-morrison.com>
2020-05-23 07:36:16 +0100

    ex16

---
+++ ch7/cmd/ex16/eval/eval.go
@@ -0,0 +1,63 @@
+// Package eval provides an expression evaluator.
+package eval
+
+import (
+	"fmt"
+	"math"
+)
+
+type Env map[Var]float64
+
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
+func (l literal) Eval(env Env) float64 {
+	return float64(l)
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
+}
+
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	}
+	panic(fmt.Sprintf("unsupported function call: %s", c.fn))
+}
+
+func (m min) Eval(env Env) float64 {
+	min := m[0].Eval(env)
+	for i := 1; i < len(m); i++ {
+		if next := m[i].Eval(env); next < min {
+			min = next
+		}
+	}
+	return min
+}
---
+++ ch7/cmd/ex16/webcalc/webcalc.go
@@ -0,0 +1,69 @@
+// Write a web-based calculator program.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"the_go_programming_language/ch7/cmd/ex16/eval"
+)
+
+func main() {
+	http.HandleFunc("/", evaluate)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func evaluate(w http.ResponseWriter, r *http.Request) {
+	URLExpr := r.FormValue("expr")
+	if URLExpr == "" {
+		fmt.Fprintf(w, "usage: /?expr=sin(x)+y&x=30&y=12\nDo not include spaces.")
+		return
+	}
+	// + is used to represent spaces in query strings and must be restored
+	URLExpr = strings.ReplaceAll(URLExpr, " ", "+")
+	expr, vars, err := parseAndCheck(URLExpr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	env, err := parseEnv(vars, r.Form)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "%.6g", expr.Eval(env))
+}
+
+// parseAndCheck parses the URL expression and returns a map of its variables to be populated
+func parseAndCheck(URLExpr string) (expr eval.Expr, vars map[eval.Var]bool, err error) {
+	vars = make(map[eval.Var]bool)
+	expr, err = eval.Parse(URLExpr)
+	if err != nil {
+		return nil, nil, fmt.Errorf("parsing %q: %v", URLExpr, err)
+	}
+	expr.Check(vars)
+	if err != nil {
+		return nil, nil, fmt.Errorf("checking %q: %v", URLExpr, err)
+	}
+	return
+}
+
+// parseEnv populates a map of expression variables with their corresponding values from the query
+func parseEnv(vars map[eval.Var]bool, form url.Values) (env eval.Env, err error) {
+	env = make(eval.Env)
+	for k := range vars {
+		def, ok := form[k.String()]
+		if !ok {
+			return nil, fmt.Errorf("variable %q is not defined", k)
+		}
+		val, err := strconv.ParseFloat(def[0], 64)
+		if err != nil {
+			return nil, fmt.Errorf("couldn't parse variable %q: %s", k, def)
+		}
+		env[k] = val
+	}
+	return
+}
